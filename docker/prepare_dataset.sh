bba71f0b1f14918bc52e4b80895b6a9066a3532f
breadbread1984 <breadbread1984@163.com>
2026-01-09 13:20:56 +0800

    upadte

--- docker/prepare_dataset.sh
+++ docker/prepare_dataset.sh
@@ -5,6 +5,7 @@ set -u
 set -o pipefail
 
 # setup swtpm
+mkdir -p /run/swtpm
 mkdir -p /tmp/mytpm
 swtpm_setup --tpmstate /tmp/mytpm --tpm2 --overwrite \
   --create-ek-cert \
@@ -12,6 +13,8 @@ swtpm_setup --tpmstate /tmp/mytpm --tpm2 --overwrite \
   --logfile /tmp/mytpm/swtpm.log \
   --log level=10
 swtpm socket --tpmstate dir=/tmp/mytpm --tpm2 --server type=tcp,port=2321 --ctrl type=tcp,port=2322 --flags not-need-init,startup-clear &
+# wait for swtpm service's ready
+sleep 5
 tpm2_startup -c
 
 # decode trainset
