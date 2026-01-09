fed48482f45cc257331e7ce12bcb05c0ee9624f7
breadbread1984 <breadbread1984@163.com>
2026-01-09 13:41:18 +0800

    update

--- docker/prepare_dataset.sh
+++ docker/prepare_dataset.sh
@@ -4,19 +4,6 @@ set -e
 set -u
 set -o pipefail
 
-# setup swtpm
-mkdir -p /run/swtpm
-mkdir -p /tmp/mytpm
-swtpm_setup --tpmstate /tmp/mytpm --tpm2 --overwrite \
-  --create-ek-cert \
-  --create-platform-cert \
-  --logfile /tmp/mytpm/swtpm.log \
-  --log level=10
-swtpm socket --tpmstate dir=/tmp/mytpm --tpm2 --server type=tcp,port=2321 --ctrl type=tcp,port=2322 --flags not-need-init,startup-clear &
-# wait for swtpm service's ready
-sleep 5
-tpm2_startup -c
-
 # decode trainset
 mkdir -p /tmp/decrypted/decoded_dataset
 for file in /app/encoded_dataset/*; do
