8ed1031f2826e4f60ea2f2c9117f2eab4a16cde9
breadbread1984 <breadbread1984@163.com>
2026-01-09 14:22:04 +0800

    update

--- docker/prepare_dataset.sh
+++ docker/prepare_dataset.sh
@@ -4,12 +4,12 @@ set -e
 set -u
 set -o pipefail
 
+mkdir -p /tmp/decrypted
 # decode trainset
-mkdir -p /tmp/decrypted/decoded_dataset
 for file in /app/encoded_dataset/*; do
 	if [ -f "$file" ]; then
 		filename="${file##*/}"
-		bash decrypt.sh "$file" /tmp/decrypted/decoded_dataset/"${filename%.*}"
+		bash decrypt.sh "$file" /tmp/decrypted/"${filename%.*}"
 	fi
 done
 
